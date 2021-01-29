04124b2768e70b0bd688f619583d3f0ec22cd3ef
Martin Gulliksson <martin@gullik.cc>
2021-01-29 09:46:17 +0100

    Use 'with socket'
    Change-Id: I21598262f2fc83490fe3f7ef4e6bdd78a2282c56

--- regea.py
+++ regea.py
@@ -12,7 +12,7 @@ socketAddress = "/tmp/regea.socket"
 workerExecutable = "./regea_worker.py"
 nWorkersMax = os.cpu_count()
 socketBufferSize = 1024
-socketTimeout = 600  # Seconds
+workerTimeout = 3600  # Seconds
 verbose = True
 
 outputFilenamePatterns = "regea.report.patterns"
@@ -53,64 +53,71 @@ def main(argv):
     except OSError:
         if os.path.exists(socketAddress):
             raise
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.bind(socketAddress)
-    sock.listen(1)
 
-    # Generate regex patterns using EA
-    nWorkersActive = 0
-    iLine = 0
-    for fileContent in fileContents:
-        for line in fileContent:
-            print(f"[{time.time() - timeStart:.3f}] Progress: {100 * (iLine) / nLines:.2f}% ({(iLine + 1)}/{nLines})")
-            for pattern in patterns:
-                if pattern.search(line) is not None:
-                    break
-            else:
-                if nWorkersActive == nWorkersMax:
-                    connection, _ = sock.accept()
-                    data = b""
-                    while True:
-                        chunk = connection.recv(socketBufferSize)
-                        if chunk:
-                            data += chunk
-                        else:
-                            break
-                    patternString = data.splitlines()[0].decode()
-                    if verbose:
-                        print(f"[{time.time() - timeStart:.3f}] Generated pattern: '{patternString}'")
-                    patterns.add(regex.compile(patternString))
-                    nWorkersActive -= 1
-                if verbose:
-                    print(f"[{time.time() - timeStart:.3f}] Generating pattern to match string: '{line}'")
-                worker = subprocess.Popen([workerExecutable] + inputFiles, stdin=subprocess.PIPE)
-                worker.stdin.write(f"{line}\n".encode())
-                worker.stdin.flush()
-                worker.stdin.close()
-                nWorkersActive += 1
-            iLine += 1
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+        sock.bind(socketAddress)
+        sock.listen(1)
 
-    # Wait for all workers to exit
-    print(f"[{time.time() - timeStart:.3f}] Waiting for all worker processes to exit...")
-    sock.settimeout(socketTimeout)
-    while nWorkersActive:
-        try:
-            connection, _ = sock.accept()
-            data = b""
-            while True:
-                chunk = connection.recv(socketBufferSize)
-                if chunk:
-                    data += chunk
+        # Generate regex patterns using EA
+        nWorkersActive = 0
+        iLine = 0
+        for fileContent in fileContents:
+            for line in fileContent:
+                print(
+                    f"[{time.time() - timeStart:.3f}] Progress: {100 * (iLine) / nLines:.2f}% ({(iLine + 1)}/{nLines})"
+                )
+                for pattern in patterns:
+                    if pattern.search(line) is not None:
+                        break
                 else:
-                    break
-            patternString = data.splitlines()[0].decode()
-            if verbose:
-                print(f"[{time.time() - timeStart:.3f}] Generated pattern: '{patternString}'")
-            patterns.add(regex.compile(patternString))
+                    if nWorkersActive == nWorkersMax:
+                        connection, _ = sock.accept()
+                        data = b""
+                        while True:
+                            chunk = connection.recv(socketBufferSize)
+                            if chunk:
+                                data += chunk
+                            else:
+                                break
+                        patternString = data.splitlines()[0].decode()
+                        if verbose:
+                            print(f"[{time.time() - timeStart:.3f}] Generated pattern: '{patternString}'")
+                        patterns.add(regex.compile(patternString))
+                        nWorkersActive -= 1
+                    if verbose:
+                        print(f"[{time.time() - timeStart:.3f}] Generating pattern to match string: '{line}'")
+                    worker = subprocess.Popen([workerExecutable] + inputFiles, stdin=subprocess.PIPE)
+                    worker.stdin.write(f"{line}\n".encode())
+                    worker.stdin.flush()
+                    worker.stdin.close()
+                    nWorkersActive += 1
+                iLine += 1
+
+        # Wait for all workers to exit
+        print(
+            f"[{time.time() - timeStart:.3f}] Waiting up to {workerTimeout} seconds for all worker processes to exit..."
+        )
+        sock.settimeout(workerTimeout)
+        try:
+            while nWorkersActive:
+                if verbose:
+                    print(f"[{time.time() - timeStart:.3f}] Waiting for {nWorkersActive} worker process(es)...")
+                connection, _ = sock.accept()
+                data = b""
+                while True:
+                    chunk = connection.recv(socketBufferSize)
+                    if chunk:
+                        data += chunk
+                    else:
+                        break
+                patternString = data.splitlines()[0].decode()
+                if verbose:
+                    print(f"[{time.time() - timeStart:.3f}] Generated pattern: '{patternString}'")
+                patterns.add(regex.compile(patternString))
+                nWorkersActive -= 1
         except socket.timeout:
-            print(f"[{time.time() - timeStart:.3f}] Timed out while waiting for worker process")
+            print(f"[{time.time() - timeStart:.3f}] Timed out waiting for {nWorkersActive} worker process(es)")
             pass
-        nWorkersActive -= 1
 
     # Calculate frequency means and variances
     print(f"[{time.time() - timeStart:.3f}] Calculating frequency means and variances...")
