165c6c183439bbdc02f347a644cedddbb1f0fe6a
Martin Gulliksson <martin@gullik.cc>
2021-02-09 09:25:49 +0100

    Missing lines instead of patterns in diff
    Change-Id: Ieca32afc309b503e68443d5848bfec7dd9ca06e3

--- regea_check.py
+++ regea_check.py
@@ -9,17 +9,24 @@ threshold = 1.0  # Number of standard deviations
 
 
 def main(argv):
-    if len(sys.argv) < 2:
-        print(f"usage: {sys.argv[0]} FILE...")
+    if len(sys.argv) < 3:
+        print(f"usage: {sys.argv[0]} ERRORFILE REFERENCEFILE...")
         return 1
 
     # Load input files
-    inputFiles = sys.argv[1:]
-    fileContents = [None] * len(inputFiles)
-    for iFile in range(len(inputFiles)):
-        with open(inputFiles[iFile], "r") as f:
-            fileContents[iFile] = f.read().splitlines()
-        fileContents[iFile] = list(filter(None, fileContents[iFile]))
+    errorFile = sys.argv[1]
+    errorFileContents = [None]
+    with open(errorFile, "r") as f:
+        errorFileContents = f.read().splitlines()
+    errorFileContents = list(filter(None, errorFileContents))
+
+    # Load input files
+    referenceFiles = sys.argv[2:]
+    referenceFileContents = [None] * len(referenceFiles)
+    for iFile in range(len(referenceFiles)):
+        with open(referenceFiles[iFile], "r") as f:
+            referenceFileContents[iFile] = f.read().splitlines()
+        referenceFileContents[iFile] = list(filter(None, referenceFileContents[iFile]))
 
     # Load training result
     patternStrings = []
@@ -37,40 +44,50 @@ def main(argv):
         frequencyStddevs[iPattern] = float(patternFrequencies[iPattern].split()[1])
 
     # Check for discrepancies
-    for iFile in range(len(inputFiles)):
-        linesMatched = [False] * len(fileContents[iFile])
-        frequenciesAboveReference = [False] * len(patternStrings)
-        frequenciesBelowReference = [False] * len(patternStrings)
-        for iPattern in range(len(patternStrings)):
-            pattern = regex.compile(patternStrings[iPattern])
-            frequency = 0
-            for iLine in range(len(fileContents[iFile])):
-                if pattern.search(fileContents[iFile][iLine]) is not None:
-                    frequency += 1
-                    linesMatched[iLine] = True
-            if frequency > frequencyMeans[iPattern] + threshold * frequencyStddevs[iPattern]:
-                frequenciesAboveReference[iPattern] = True
-            elif frequency < frequencyMeans[iPattern] - threshold * frequencyStddevs[iPattern]:
-                frequenciesBelowReference[iPattern] = True
+    linesMatched = [False] * len(errorFileContents)
+    frequenciesAboveReference = [False] * len(patternStrings)
+    frequenciesBelowReference = [False] * len(patternStrings)
+    for iPattern in range(len(patternStrings)):
+        pattern = regex.compile(patternStrings[iPattern])
+        frequency = 0
+        for iLine in range(len(errorFileContents)):
+            if pattern.search(errorFileContents[iLine]) is not None:
+                frequency += 1
+                linesMatched[iLine] = True
+        if frequency > frequencyMeans[iPattern] + threshold * frequencyStddevs[iPattern]:
+            frequenciesAboveReference[iPattern] = True
+        elif frequency < frequencyMeans[iPattern] - threshold * frequencyStddevs[iPattern]:
+            frequenciesBelowReference[iPattern] = True
 
-        # Generate diff
-        diffFileContents = set()
-        for iLine in range(len(fileContents[iFile])):
-            if not linesMatched[iLine]:
-                diffFileContents.add(f"> {fileContents[iFile][iLine]}")
-        for iPattern in range(len(patternStrings)):
-            if frequenciesBelowReference[iPattern]:
-                diffFileContents.add(f"< {patternStrings[iPattern]}")
-            if frequenciesAboveReference[iPattern]:
-                pattern = regex.compile(patternStrings[iPattern])
-                for line in fileContents[iFile]:
-                    if pattern.search(line) is not None:
-                        diffFileContents.add(f"> {line}")
+    # Generate diff
+    diffFileContents = set()
+    for iLine in range(len(errorFileContents)):
+        if not linesMatched[iLine]:
+            diffFileContents.add(f"> {errorFileContents[iLine]}")
+    for iPattern in range(len(patternStrings)):
+        pattern = regex.compile(patternStrings[iPattern], regex.MULTILINE)
 
-        # Write results to disk
-        with open(f"{inputFiles[iFile]}.diff", "w") as diffFile:
-            diffFile.write("\n".join(sorted(list(diffFileContents))))
-            diffFile.write("\n")
+        errorFileMatches = set()
+        for iLine in range(len(errorFileContents)):
+            if pattern.match(errorFileContents[iLine]) is not None:
+                errorFileMatches.add(errorFileContents[iLine])
+
+        referenceFileMatches = set()
+        for iFile in range(len(referenceFileContents)):
+            for iLine in range(len(referenceFileContents[iFile])):
+                if pattern.match(referenceFileContents[iFile][iLine]) is not None:
+                    referenceFileMatches.add(referenceFileContents[iFile][iLine])
+
+        if frequenciesBelowReference[iPattern] or frequenciesAboveReference[iPattern]:
+            for line in errorFileMatches.difference(referenceFileMatches):
+                diffFileContents.add(f"> {line}")
+            for line in referenceFileMatches.difference(errorFileMatches):
+                diffFileContents.add(f"< {line}")
+
+    # Write results to disk
+    with open(f"{errorFile}.diff", "w") as diffFile:
+        diffFile.write("\n".join(sorted(list(diffFileContents))))
+        diffFile.write("\n")
 
 
 if __name__ == "__main__":
