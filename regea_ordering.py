4d51b83341cfa984327b30c4f317ced1fb88c302
Martin Gulliksson <martin@gullik.cc>
2021-02-12 13:44:49 +0100

    Naive ordering check
    Change-Id: Ia57d0edc8bd7eade3bd2099569680e1d60969c48

--- regea_ordering.py
+++ regea_ordering.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import regex
 import sys
 
 inputFilenamePatterns = "regea.report.patterns"
@@ -14,7 +15,7 @@ def main(argv):
     errorFileContents = [None]
     with open(errorFile, "r") as f:
         errorFileContents = f.read().splitlines()
-    errorFileContents = list(filter(None, errorFileContents))
+    errorFileContents = "\n".join(list(filter(None, errorFileContents)))
 
     # Load input files
     referenceFiles = sys.argv[2:]
@@ -22,12 +23,56 @@ def main(argv):
     for iFile in range(len(referenceFiles)):
         with open(referenceFiles[iFile], "r") as f:
             referenceFileContents[iFile] = f.read().splitlines()
-        referenceFileContents[iFile] = list(filter(None, referenceFileContents[iFile]))
+        referenceFileContents[iFile] = "\n".join(list(filter(None, referenceFileContents[iFile])))
 
     # Load training result
     patternStrings = []
     with open(inputFilenamePatterns, "r") as inputFilePatterns:
         patternStrings.extend(inputFilePatterns.read().splitlines())
+
+    # Generate ordering rules
+    rules = set()
+    for iPattern in range(len(patternStrings)):
+        print(f"iPattern = {iPattern}/{len(patternStrings)}, nRules = {len(rules)}")
+        pattern = regex.compile(patternStrings[iPattern], regex.MULTILINE)
+        for iPatternOther in range(len(patternStrings)):
+            if iPattern == iPatternOther:
+                continue
+            patternOther = regex.compile(patternStrings[iPatternOther], regex.MULTILINE)
+
+            for iFile in range(len(referenceFiles)):
+                match = pattern.search(referenceFileContents[iFile])
+                matchOther = patternOther.search(referenceFileContents[iFile])
+                if match is None or matchOther is None or matchOther.span()[0] < match.span()[0]:
+                    break
+            else:
+                rules.add((pattern, patternOther))  # Add rule that pattern is always matches before patternOther
+
+    # Check rules for error log
+    print("Running checks...")
+    for rule in rules:
+        pattern = rule[0]
+        patternOther = rule[1]
+
+        # Sanity check
+        # for iFile in range(len(referenceFiles)):
+        #    match = pattern.search(referenceFileContents[iFile])
+        #    matchOther = patternOther.search(referenceFileContents[iFile])
+        #    if match is None or matchOther is None or matchOther.span()[0] < match.span()[0]:
+        #        print(f"Generated rule {rule} is invalid! How could this happen??")
+        #        sys.exit(1)
+
+        match = pattern.search(errorFileContents)
+        matchOther = patternOther.search(errorFileContents)
+        if match is None or matchOther is None:
+            continue
+        if matchOther.span()[0] < match.span()[0]:
+            print("Error log violates ordering rule:")
+            print(f"    Pattern '{pattern.pattern}' should match before '{patternOther.pattern}'")
+            print(
+                f"    String '{match.string[match.span()[0] : match.span()[1]]}' should come before '{matchOther.string[matchOther.span()[0] : matchOther.span()[1]]}'"
+            )
+            print("")
 
 
 if __name__ == "__main__":
