bcad46ac560d8906ea5bffd1bb7510d9084c54ea
Martin Gulliksson <martin@gullik.cc>
2021-02-15 12:54:32 +0100

    Calculate pattern indices
    Change-Id: I9ebda57f86930434c4be4f0040a9c57a78c7e5b3

--- regea_ordering.py
+++ regea_ordering.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 import regex
 import sys
 
@@ -15,7 +16,7 @@ def main(argv):
     errorFileContents = [None]
     with open(errorFile, "r") as f:
         errorFileContents = f.read().splitlines()
-    errorFileContents = "\n".join(list(filter(None, errorFileContents)))
+    errorFileContents = list(filter(None, errorFileContents))
 
     # Load input files
     referenceFiles = sys.argv[2:]
@@ -23,56 +24,35 @@ def main(argv):
     for iFile in range(len(referenceFiles)):
         with open(referenceFiles[iFile], "r") as f:
             referenceFileContents[iFile] = f.read().splitlines()
-        referenceFileContents[iFile] = "\n".join(list(filter(None, referenceFileContents[iFile])))
+        referenceFileContents[iFile] = list(filter(None, referenceFileContents[iFile]))
 
     # Load training result
     patternStrings = []
     with open(inputFilenamePatterns, "r") as inputFilePatterns:
         patternStrings.extend(inputFilePatterns.read().splitlines())
-
-    # Generate ordering rules
-    rules = set()
+    patterns = [None] * len(patternStrings)
     for iPattern in range(len(patternStrings)):
-        print(f"iPattern = {iPattern}/{len(patternStrings)}, nRules = {len(rules)}")
-        pattern = regex.compile(patternStrings[iPattern], regex.MULTILINE)
-        for iPatternOther in range(len(patternStrings)):
-            if iPattern == iPatternOther:
-                continue
-            patternOther = regex.compile(patternStrings[iPatternOther], regex.MULTILINE)
+        patterns[iPattern] = regex.compile(patternStrings[iPattern])
 
-            for iFile in range(len(referenceFiles)):
-                match = pattern.search(referenceFileContents[iFile])
-                matchOther = patternOther.search(referenceFileContents[iFile])
-                if match is None or matchOther is None or matchOther.span()[0] < match.span()[0]:
+    # Convert log entries to lists of pattern indices
+    errorPatternIndices = np.array([np.nan] * len(errorFileContents))
+    for iLine in range(len(errorFileContents)):
+        for iPattern in range(len(patterns)):
+            if patterns[iPattern].match(errorFileContents[iLine]) is not None:
+                errorPatternIndices[iLine] = iPattern
+                break
+    errorPatternIndices = errorPatternIndices[~(np.isnan(errorPatternIndices))]
+    assert not np.isnan(errorPatternIndices).any()
+
+    referencePatternIndices = [None] * len(referenceFiles)
+    for iFile in range(len(referenceFiles)):
+        referencePatternIndices[iFile] = np.array([np.nan] * len(referenceFileContents[iFile]))
+        for iLine in range(len(referenceFileContents[iFile])):
+            for iPattern in range(len(patterns)):
+                if patterns[iPattern].match(referenceFileContents[iFile][iLine]) is not None:
+                    referencePatternIndices[iFile][iLine] = iPattern
                     break
-            else:
-                rules.add((pattern, patternOther))  # Add rule that pattern is always matches before patternOther
-
-    # Check rules for error log
-    print("Running checks...")
-    for rule in rules:
-        pattern = rule[0]
-        patternOther = rule[1]
-
-        # Sanity check
-        # for iFile in range(len(referenceFiles)):
-        #    match = pattern.search(referenceFileContents[iFile])
-        #    matchOther = patternOther.search(referenceFileContents[iFile])
-        #    if match is None or matchOther is None or matchOther.span()[0] < match.span()[0]:
-        #        print(f"Generated rule {rule} is invalid! How could this happen??")
-        #        sys.exit(1)
-
-        match = pattern.search(errorFileContents)
-        matchOther = patternOther.search(errorFileContents)
-        if match is None or matchOther is None:
-            continue
-        if matchOther.span()[0] < match.span()[0]:
-            print("Error log violates ordering rule:")
-            print(f"    Pattern '{pattern.pattern}' should match before '{patternOther.pattern}'")
-            print(
-                f"    String '{match.string[match.span()[0] : match.span()[1]]}' should come before '{matchOther.string[matchOther.span()[0] : matchOther.span()[1]]}'"
-            )
-            print("")
+        assert not np.isnan(referencePatternIndices[iFile]).any()
 
 
 if __name__ == "__main__":
