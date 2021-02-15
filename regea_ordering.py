187262d4206a78210f877f68f0d3ef21cebe8c3b
Martin Gulliksson <martin@gullik.cc>
2021-02-15 16:25:13 +0100

    Add some logic rules for ordering checks
    Change-Id: I95e1f49f8ff316a10c33a8fa38e0097f28de845a

--- regea_ordering.py
+++ regea_ordering.py
@@ -1,9 +1,76 @@
 #!/usr/bin/env python3
+import enum
 import numpy as np
+import random
 import regex
 import sys
 
 inputFilenamePatterns = "regea.report.patterns"
+
+
+class RuleType(enum.IntEnum):
+    BEFORE_ALL = 0
+    AFTER_ALL = 1
+    BEFORE_ANY = 2
+    AFTER_ANY = 3
+    DIRECTLY_BEFORE = 4
+    DIRECTLY_AFTER = 5
+
+
+class Rule:
+    def __init__(self, patternStrings):
+        self.iPattern = random.randint(0, len(patternStrings) - 1)
+        self.iPatternOther = random.randint(0, len(patternStrings) - 1)
+        self.patternString = patternStrings[self.iPattern]
+        self.patternStringOther = patternStrings[self.iPatternOther]
+        self.type = random.randint(0, len(RuleType) - 1)
+
+    def evaluate(self, patternIndices):
+        if self.iPattern == self.iPatternOther:
+            return False
+        iPatternMatches = np.where(patternIndices == self.iPattern)[0]
+        iPatternOtherMatches = np.where(patternIndices == self.iPatternOther)[0]
+        if not len(iPatternMatches) or not len(iPatternOtherMatches):
+            return False
+
+        if self.type == RuleType.BEFORE_ALL:
+            if not max(iPatternMatches) < min(iPatternOtherMatches):
+                return False
+        elif self.type == RuleType.AFTER_ALL:
+            if not min(iPatternMatches) > max(iPatternOtherMatches):
+                return False
+        elif self.type == RuleType.BEFORE_ANY:
+            if not min(iPatternMatches) < min(iPatternOtherMatches):
+                return False
+        elif self.type == RuleType.AFTER_ANY:
+            if not max(iPatternMatches) > max(iPatternOtherMatches):
+                return False
+        elif self.type == RuleType.DIRECTLY_BEFORE:
+            if len(iPatternOtherMatches) < len(iPatternMatches):
+                return False
+            for iPatternMatch in iPatternMatches:
+                if not len(np.where(iPatternOtherMatches - iPatternMatch == 1)[0]):
+                    return False
+        elif self.type == RuleType.DIRECTLY_AFTER:
+            for iPatternMatch in iPatternMatches:
+                if not len(np.where(iPatternOtherMatches - iPatternMatch == -1)[0]):
+                    return False
+        else:
+            raise NotImplementedError(f"Unknown rule type {RuleType(self.type).name}")
+
+        return True
+
+    def __str__(self):
+        return f"Pattern '{self.patternString}' always matches {RuleType(self.type).name} pattern '{self.patternStringOther}'"
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 def main(argv):
@@ -32,27 +99,48 @@ def main(argv):
         patternStrings.extend(inputFilePatterns.read().splitlines())
     patterns = [None] * len(patternStrings)
     for iPattern in range(len(patternStrings)):
-        patterns[iPattern] = regex.compile(patternStrings[iPattern])
+        patterns[iPattern] = regex.compile(patternStrings[iPattern], regex.MULTILINE)
 
     # Convert log entries to lists of pattern indices
-    errorPatternIndices = np.array([np.nan] * len(errorFileContents))
+    errorPatternIndices = np.array([-1] * len(errorFileContents), dtype=np.int64)
     for iLine in range(len(errorFileContents)):
         for iPattern in range(len(patterns)):
             if patterns[iPattern].match(errorFileContents[iLine]) is not None:
                 errorPatternIndices[iLine] = iPattern
                 break
-    errorPatternIndices = errorPatternIndices[~(np.isnan(errorPatternIndices))]
-    assert not np.isnan(errorPatternIndices).any()
+    errorPatternIndices = errorPatternIndices[errorPatternIndices != -1]
+    assert not len(np.where(errorPatternIndices == -1)[0])
 
     referencePatternIndices = [None] * len(referenceFiles)
     for iFile in range(len(referenceFiles)):
-        referencePatternIndices[iFile] = np.array([np.nan] * len(referenceFileContents[iFile]))
+        referencePatternIndices[iFile] = np.array([-1] * len(referenceFileContents[iFile]), dtype=np.int64)
         for iLine in range(len(referenceFileContents[iFile])):
             for iPattern in range(len(patterns)):
                 if patterns[iPattern].match(referenceFileContents[iFile][iLine]) is not None:
                     referencePatternIndices[iFile][iLine] = iPattern
                     break
-        assert not np.isnan(referencePatternIndices[iFile]).any()
+        assert not len(np.where(referencePatternIndices[iFile] == -1)[0])
+
+    errorFileContentsJoined = "\n".join(errorFileContents)
+    rules = set()
+    while True:
+        rule = Rule(patternStrings)
+        for iFile in range(len(referenceFiles)):
+            if not rule.evaluate(referencePatternIndices[iFile]):
+                break
+        else:
+            if rule not in rules:
+                rules.add(rule)
+                if (
+                    rule.iPattern in errorPatternIndices
+                    and rule.iPatternOther in errorPatternIndices
+                    and not rule.evaluate(errorPatternIndices)
+                ):
+                    print(
+                        f"{rule}\n{patterns[rule.iPattern].findall(errorFileContentsJoined)}\n{patterns[rule.iPatternOther].findall(errorFileContentsJoined)}\n"
+                    )
+                    print(len(rules))
+    return 0
 
 
 if __name__ == "__main__":
